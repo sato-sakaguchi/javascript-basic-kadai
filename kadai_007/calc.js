c160065f6ba52b22b50305425c0cf108cc987573
sato-sakaguchi <135315904+sato-sakaguchi@users.noreply.github.com>
2023-12-13 13:30:14 +0900

    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+// 変数numに1~100までのランダムな整数を代入する
+let num = Math.floor(Math.random() * 100) + 1;
+// 変数numの値を出力する（確認用）
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+} 
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+else {
+    console.log(num);
+}
